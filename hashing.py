1855e500ebc75dedd251e9f4c7d4a85532a857f0
DevJeb <124264016+Shaikst@users.noreply.github.com>
2023-12-31 00:57:18 +0500

    Update hashing.py

--- hashing.py
+++ hashing.py
@@ -3,7 +3,7 @@ import json
 response = urllib.request.urlopen("https://raw.githubusercontent.com/Shaikst/hashing/main/Hashing_dictionary.json")
 hashing = json.loads(response.read().decode('utf-8'))
 print("██████╗░███████╗██╗░░░██╗░░░░░██╗███████╗██████╗░\n██╔══██╗██╔════╝██║░░░██║░░░░░██║██╔════╝██╔══██╗\n██║░░██║█████╗░░╚██╗░██╔╝░░░░░██║█████╗░░██████╦╝\n██║░░██║██╔══╝░░░╚████╔╝░██╗░░██║██╔══╝░░██╔══██╗\n██████╔╝███████╗░░╚██╔╝░░╚█████╔╝███████╗██████╦╝\n╚═════╝░╚══════╝░░░╚═╝░░░░╚════╝░╚══════╝╚═════╝░")
-print("Разработанно DevJeb, используется 64-ая система")
+print("Разработанно DevJeb, используется 64-ая система\n"+response.read().decode('utf-8').split("\n")[2])
 a=input('Введите текст: ')
 b=list(a)
 result=""
